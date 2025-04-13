80b2a100a026bf966f8a59211eca9b62be564b0a
Barát Péter <125590088+BaratP@users.noreply.github.com>
2025-04-13 18:39:59 +0200

    fix

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,42 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: 'main',
+    loadChildren: () => import('./pages/main/main.module').then(m => m.MainModule)
+  },
+  {
+    path: 'gallery',
+    loadChildren: () => import('./pages/gallery/gallery.module').then(m => m.GalleryModule)
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./pages/login/login.module').then(m => m.LoginModule)
+  },
+  {
+    path: 'register',
+    loadChildren: () => import('./pages/register/register.module').then(m => m.RegisterModule)
+  },
+  {
+    path: 'not-found',
+    loadChildren: () => import('./pages/not-found/not-found.module').then(m => m.NotFoundModule)
+  },
+  {
+    path: '',
+    redirectTo: '/main',
+    pathMatch:'full'
+  },
+  {
+    path: '**',
+    redirectTo: '/not-found'
+  },
+  
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/shared/interfaces/Furniture.ts
@@ -0,0 +1,6 @@
+export interface Furniture{
+    id: string;
+    type: string;
+    userid: string;
+    date: Date;
+  }
